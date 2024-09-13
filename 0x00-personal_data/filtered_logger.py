3cc52ffbe7923c1246d6147f0f9e05e5acaa412c
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-13 15:35:37 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,3 +17,26 @@ def filter_datum(fields: List[str], redaction: str,
                 f'{f}={redaction}{separator}', message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+        REDACTION = "***"
+        FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+        SEPARATOR = ";"
+
+        def __init__(self):
+            super(RedactingFormatter, self).__init__(self.FORMAT)
+            self.fields = fields
+
+        def format(self, record: logging.LogRecord) -> str:
+            """ filter values in incoming log records using filter_datum"""
+            record.msg = filter_datum(self.fields, self.REDACTION,
+                                        record.getMessage(), self.SEPARATOR)
+            return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == "__main__":
+    main()
