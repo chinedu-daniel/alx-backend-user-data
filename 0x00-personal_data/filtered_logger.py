536fbeee16dde1360153e5c930c2261cca40d5ac
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-13 15:08:22 +0100

    filtered_logger.py & README.md

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
+"""
+regexing
+"""
 
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     """
     regex-ing
     """
     for f in fields:
-        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+        message = re.sub(f'{f}=.*?{separator}',
+                f'{f}={redaction}{separator}', message)
 
     return message
