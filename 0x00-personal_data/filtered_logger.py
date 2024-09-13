d00d7b6c9f8ffedfdc7be611d06549f3599cb394
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-13 14:45:13 +0100

    filtered_logger.py & README.md

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     regex-ing
     """
-    pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
-    return re.sub(pattern, redaction, message)
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+
+    return message
