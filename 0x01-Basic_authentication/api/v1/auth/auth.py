d91c804d33ea9f6f7b27f9b41cf7addea681a19a
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-19 06:02:52 +0100

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Basic Authentication"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method"""
+
+        if not path:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        if normalized_path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """public method"""
+
+        if request is None:
+            return None
+
+        if request.headers.get("Authorization") is None:
+            return None
+
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method"""
+        return None
