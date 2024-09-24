5696e7d301f40b6bc5f888dcf1d3774cb578f988
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-24 11:54:09 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,30 @@
 import bcrypt
 
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers new user
+            Args:
+                - email: user's email
+                - password: user's password
+            Return:
+                - User instance created
+        """
+        db = self._db
+        try:
+            user = db.find_user_by(email=email)
+        except NoResultFound:
+            user = db.add_user(email, _hash_password(password))
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
+
 def _hash_password(self, password: str) -> bytes:
     # Convert the password string to bytes
     password_bytes = password.encode('utf-8')
