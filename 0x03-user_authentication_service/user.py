418386512c112de69c40e7fa3261b00fb49d967d
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-24 08:02:20 +0100

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from database_setup import Base  # Import Base from your setup file
+
+
+# 1. Define the User model class
+class User(Base):
+    # 2. Specify the name of the table in the database
+    __tablename__ = 'users'
+
+    # 3. Define the columns in the users table
+    id = Column(Integer, primary_key=True)  # primary key
+    email = Column(String(250), nullable=False)  # string for email
+    hashed_password = Column(String(250), nullable=False)  # string for password
+    session_id = Column(String(250), nullable=True)  # string for session ID
+    reset_token = Column(String(250), nullable=True)  # string for reset token
