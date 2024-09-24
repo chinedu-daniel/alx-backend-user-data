66bcdd8b2b536808138b479dfde3c91ab3d65d2a
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-24 11:37:15 +0100

    db.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Hash password"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    # Convert the password string to bytes
+    password_bytes = password.encode('utf-8')
+    
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    
+    # Return the hashed password as bytes
+    return hashed_password
